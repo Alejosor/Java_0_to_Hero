9e09cf96ba9c8877b6df8b57bab20b30a26f7ab2
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-14 18:13:54 -0500

    Create julianb0315.java

---
+++ Ejercicios/julianb0315.java
@@ -0,0 +1,7 @@
+public class JulianB0315 {
+    public static void main(String[] args) {
+        System.out.println("Git User:JulianB0315");
+        System.out.println("Profile link:https://github.com/JulianB0315");
+    }
+}
+//Sigue el repo de Python_to_hero
