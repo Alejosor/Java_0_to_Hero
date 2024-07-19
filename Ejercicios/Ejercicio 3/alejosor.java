5b069c99a655be15f5e0e3ebbb149f1d542e6393
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-07-19 16:08:39 -0500

    Ejercicio #3 - Alejosor

---
+++ Ejercicios/Ejercicio 3/alejosor.java
@@ -0,0 +1,15 @@
+public class alejosor {
+    public static void main(String[] args) {
+        String nombre = "Alejandro Gabriel Soriano Palomino";
+        int edad = 23;
+        double alturaMetros = 1.82;
+        char genero = 'M';
+        boolean estudiante = true;
+
+        System.out.println("Mi nombre es: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Estatura: " + alturaMetros);
+        System.out.println("Genero: " + genero);
+        System.out.println("¿Soy estudiante?: " + estudiante);
+    }
+}
