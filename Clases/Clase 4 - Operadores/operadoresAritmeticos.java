24863229899a511cc1cd8e228cc9ef8e839b0ac1
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-07-23 13:00:04 -0500

    Java - Clase 4 - Operadores Aritméticos Básicos **Actualización

--- Clases/Clase 4 - Operadores/operadoresAritmeticos.java
+++ Clases/Clase 4 - Operadores/operadoresAritmeticos.java
@@ -34,5 +34,50 @@ public class operadoresAritmeticos {
         //Módulo -> Sirve para obtener el resto de una división
         resultado = numero1 % numero2;
         System.out.println("El resultado del residuo es -> " + resultado);
+
+        //Datos importantes a tener en cuenta
+
+        /*
+        •El resultado es de tipo long si, al menos, uno de los operandos es de tipo long y ninguno
+        es real (float o double).
+
+            long num3 = 10l;
+            int num4 = 5;
+
+            long resultado2 = num3 + num4;
+        */
+
+
+        /*
+        El resultado es de tipo int si ninguno de los operandos es de tipo long y tampoco es real (float o double).
+
+            int num3 = 10;
+            int num4 = 5;
+
+            int resultado2 = num3 + num4;
+        */
+
+
+        /*
+        El resultado es de tipo double si, al menos, uno de los operandos es de tipo double
+
+            double num3 = 10;
+            int num4 = 5;
+
+            double resultado2 = num3 + num4;
+         */
+
+
+        /*
+        El resultado es de tipo float si, al menos,
+        uno de los operandos es de tipo float y ninguno es double.
+
+            float num3 = 10;
+            int num4 = 5;
+
+            float resultado2 = num3 + num4;
+
+        */
+
     }
 }
