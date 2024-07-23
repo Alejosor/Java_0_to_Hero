670103919cf1bd0cdcdb5150b0dc545f3be5dc72
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-07-23 17:46:23 -0500

    Java - Clase 4 - Operadores de Comparación

---
+++ Clases/Clase 4 - Operadores/operadoresComparacion.java
@@ -0,0 +1,38 @@
+public class operadoresComparacion {
+    public static void main(String[] args) {
+        /*
+        OPERADORES DE COMPARACIÓN
+        - Igual que '==': Sirve para comprobar que una expresión o varias variables son iguales.
+        - Distinto que '!=': Sirve para comprobar que una expresión o varias variables son distintos.
+        - Menor que '<': Sirve para comprobar que una expresión o varias variables son menores que otras.
+        - Mayor que '>': Sirve para comprobar que una expresión o varias variables son mayores que otras.
+        - Menor o igual que '<=': Sirve para comprobar que una expresión o varias variables son
+            menores o iguales a otras
+        - Mayor o igual que '>=': Sirve para comprobar que una expresión o varias variables son
+            mayores o iguales a otras
+         */
+
+        //Declaración de variables
+        int num1 = 5;
+        int num2 = 5;
+
+        boolean resultado = false;
+        // Operador de igualdad o igual que ==
+        System.out.println(num1 == num2); // Resultado true, porque hay igualdad
+
+        //Operador de desigualdad o distinto de !=
+        System.out.println(num1 != num2); // Resultado false, porque no hay igualdad
+
+        //Operador de menor que <
+        System.out.println(num1 < num2); // Resultado false, porque no es menor
+
+        //Operador de mayor que >
+        System.out.println(num1 > num2); // Resultado false, porque no es mayor
+
+        //Operador de menor o igual que <=
+        System.out.println(num1 <= num2); // Resultado true, porque hay igualdad
+
+        //Operador de mayor o igual que >=
+        System.out.println(num1 >= num2); // Resultado true, porque hay igualdad
+    }
+}
