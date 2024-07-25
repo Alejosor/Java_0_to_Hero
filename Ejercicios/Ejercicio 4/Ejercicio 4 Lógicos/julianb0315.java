47e81d8e71b92209ae3b5d7ddedd60295d515b0a
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-25 17:28:26 -0500

    Create julianb0315.java

---
+++ Ejercicios/Ejercicio 4/Ejercicio 4 Lógicos/julianb0315.java
@@ -0,0 +1,18 @@
+public class julianb0315 {
+    public static void main(String[] args) {
+    //AND Lógico:
+       boolean cond1 = false; 
+       boolean cond2 = false;
+       boolean and = cond1 && cond2;
+       System.out.println("Logico AND: " + and);
+    //OR Lógico:
+       boolean cond3 = true;
+       boolean cond4 = true;
+       boolean or = cond3 || cond4;
+       System.out.println("Logico OR: " + or);
+    //NOT Lógico:
+       boolean cond5 = true;
+       boolean not = !cond5;
+       System.out.println("Logico NOT: " + not);
+    }
+}
