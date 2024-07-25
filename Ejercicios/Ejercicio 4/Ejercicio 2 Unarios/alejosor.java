e247f8950b1ae007eb8ebf4e5b94b4e724417498
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-07-24 19:14:47 -0500

    Ejercicio #4 - Alejosor

---
+++ Ejercicios/Ejercicio 4/Ejercicio 2 Unarios/alejosor.java
@@ -0,0 +1,14 @@
+public class alejosor {
+    public static void main(String[] args) {
+       // Operadores Unarios
+       int num = 7;
+       num++;
+       System.out.println("Incremento: " + num);
+       num--;
+       System.out.println("Decremento: " + num);
+
+       int m = -4;
+       int negacion = -m;
+       System.out.println("Negación: " + negacion);
+    }
+}
