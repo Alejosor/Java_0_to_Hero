7a6d081ac5e8e0328b30d008d783b0875773d221
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-07-24 19:16:23 -0500

    Ejercicio #4 - Alejosor

---
+++ Ejercicios/Ejercicio 4/Ejercicio 3 Relacionales/alejosor.java
@@ -0,0 +1,16 @@
+public class alejosor {
+    public static void main(String[] args) {
+       // Operadores Relacionales
+       int p = 8, q = 8;
+       boolean igualdad = p == q;
+       boolean desigualdad = p != q;
+       System.out.println("Igualdad: " + igualdad);
+       System.out.println("Desigualdad: " + desigualdad);
+
+       int u = 12, v = 15;
+       boolean mayor = u > v;
+       boolean menorIgual = v <= u;
+       System.out.println("Mayor: " + mayor);
+       System.out.println("Menor o igual: " + menorIgual);
+    }
+}
