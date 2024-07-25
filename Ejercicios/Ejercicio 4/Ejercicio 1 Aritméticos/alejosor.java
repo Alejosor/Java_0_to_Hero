5e40121d933ce69171a537f0ce2fa17861640fb6
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-07-24 19:11:59 -0500

    Ejercicio #4 - Alejosor

---
+++ Ejercicios/Ejercicio 4/Ejercicio 1 Aritméticos/alejosor.java
@@ -0,0 +1,20 @@
+public class alejosor {
+    public static void main(String[] args) {
+       // Operadores Aritméticos
+       int x = 20, y = 10;
+       int suma = x + y;
+       int resta = x - y;
+       System.out.println("Suma: " + suma);
+       System.out.println("Resta: " + resta);
+
+       int a = 12, b = 4;
+       int producto = a * b;
+       int division = a / b;
+       System.out.println("Producto: " + producto);
+       System.out.println("División: " + division);
+
+       int n = 21;
+       int modulo = n % 5;
+       System.out.println("Módulo: " + modulo);
+    }
+}
