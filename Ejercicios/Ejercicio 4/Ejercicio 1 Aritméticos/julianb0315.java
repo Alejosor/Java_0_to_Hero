3d7cdb86c13846aadd361911d1f794f7b9c5dfd1
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-25 16:58:25 -0500

    Create julianb0315.java

---
+++ Ejercicios/Ejercicio 4/Ejercicio 1 Aritméticos/julianb0315.java
@@ -0,0 +1,22 @@
+public class julianb0315 {
+   public static void main(String[] args) {
+    //Suma y Resta:
+           int n1 = 4; 
+           int n2 = 30;
+           int sum =n1+n2;
+           int rest= n1-n2;
+           System.out.println("La suma es: " + sum);
+           System.out.println("La resta es: " + rest);
+    //Multiplicación y División:
+           int p1 = 2; 
+           int p2 = 12;
+           int pro = p1 * p2;
+           int div = p1 / p2;
+           System.out.println("El resultado de multiplicación es: " + pro);
+           System.out.println("El resultado de división  es: " + div);
+    //*Módulo:
+           int m1 = 57;
+           int modulo = m1 % 5;
+           System.out.println("El resultado de modulo es: " + modulo);
+   }
+}
