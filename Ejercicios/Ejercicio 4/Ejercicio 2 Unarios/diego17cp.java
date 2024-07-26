2d62d14da00002438d66fdb60c4d963c7ff65934
Diego Castro <diegoalbertocastropastor@gmail.com>
2024-07-26 16:17:04 -0500

    Create diego17cp.java

---
+++ Ejercicios/Ejercicio 4/Ejercicio 2 Unarios/diego17cp.java
@@ -0,0 +1,10 @@
+public class diego17cp {
+  public static void main (String[]args) {
+    int contador=6;
+    contador++; contador--;
+    System.out.println("El valor del contador luego de sus cambios es: "+contador);
+
+    boolean m=false;
+    System.out.println(!m);
+  }
+}
