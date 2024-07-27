eb48de8a90024c6baaf4f0ead2da5b7af293e682
Diego Castro <diegoalbertocastropastor@gmail.com>
2024-07-26 20:28:44 -0500

    Create diego17cp.java

---
+++ Ejercicios/Ejercicio 4/Ejercicio 4 Lógicos/diego17cp.java
@@ -0,0 +1,9 @@
+public class diego17cp {
+  public static void main (String[]args) {
+    boolean cond1=false, cond2=true;
+    boolean resu=cond1&&cond2; resu2=cond1||cond2; not=!cond1;
+    System.out.println("Resultado del ejercicio: "+resu);
+    System.out.println("Resultado del ejercicio 2: "+resu2);
+    System.out.println("Resultado del ejercicio 3: "+not);
+  }
+}
