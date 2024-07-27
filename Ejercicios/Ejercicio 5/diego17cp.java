7f505d58e3a3e50055f4b13249800e100465d3bc
Diego Castro <diegoalbertocastropastor@gmail.com>
2024-07-27 11:19:18 -0500

    Create diego17cp.java

---
+++ Ejercicios/Ejercicio 5/diego17cp.java
@@ -0,0 +1,13 @@
+public class diego17cp {
+  public static void main (String[]args) {
+    double num1=100.0;
+    int num2=num1;
+    String num3=Integer.toString(num2);
+    System.out.println(num3);
+    String str1="10"; str2="5";
+    int x=Integer.parseint(str1);
+    double y=Float.parseFloat(str2);
+    boolean resultado=x>y;
+    System.out.println(resultado);
+  }
+}
