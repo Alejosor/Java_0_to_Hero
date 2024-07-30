c72ab6d40b57047ad2a5d1ee48ccde24957ebed7
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-07-30 18:08:10 -0500

    Java - Clase 4 - Operadores de Comparación

--- Clases/Clase 4 - Operadores/operadores6Comparacion.java
+++ Clases/Clase 4 - Operadores/operadores6Comparacion.java
@@ -1,4 +1,10 @@
-public class operadoresComparacion {
+/* ╔══════════════════════════════════════╗
+   ║ Autor: Alejandro Soriano             ║
+   ║ Perfil: https://github.com/Alejosor  ║
+   ╚══════════════════════════════════════╝
+*/
+
+public class operadores6Comparacion {
     public static void main(String[] args) {
         /*
         OPERADORES DE COMPARACIÓN
